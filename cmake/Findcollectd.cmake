449358edf8c4d5374e743ee24cabeede97a34fba
kakwa <carpentier.pf@gmail.com>
2017-07-04 15:00:00 +0200

    adding the "/usr/include/collectd" (or the OS specific variant) in include path

--- cmake/Findcollectd.cmake
+++ cmake/Findcollectd.cmake
@@ -1,6 +1,8 @@
 IF(NOT COLLECTD_INCLUDE_DIR)
     FIND_PATH(COLLECTD_INCLUDE_DIR collectd.h PATH_SUFFIXES collectd/core collectd/core/daemon)
+    FIND_PATH(COLLECTD_INCLUDE_DIR_BASE core PATH_SUFFIXES collectd)
     MESSAGE(STATUS "Find Header Directory for collectd: " ${COLLECTD_INCLUDE_DIR})
+    MESSAGE(STATUS "Find Header Base Directory for collectd: " ${COLLECTD_INCLUDE_DIR_BASE})
 ENDIF()
 
 
